125c59516acd71798e749fcc1fba5ec830f77049
juliasahakyan <129150793+juliasahakyan@users.noreply.github.com>
2023-03-31 12:01:25 +0400

    Update date_conf.sh

--- date_conf.sh
+++ date_conf.sh
@@ -11,10 +11,7 @@ cat <<EOF > /var/www/html/index.html
   </body>
 </html>
 EOF
-# Add a cronjob to update the HTML file every 30 seconds
-# Save current crontab to a file
-#crontab -l > mycron
-# Add a new cronjob to the file that updates the HTML file
+
 sed -i "s#<div id=\"datetime\"></div>#<div id=\"datetime\">$(date '+%Y-%m-%d %H:%M:%S')</div>#g" /var/www/html/index.html
 
 
